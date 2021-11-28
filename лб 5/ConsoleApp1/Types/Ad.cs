86e26dcfd0149824a5caa3f670df9e60673a267b
dianazmitrevich <zmitrevichdiana@gmail.com>
2021-11-28 20:29:02 +0300

    added ad class

---
+++ лб 5/ConsoleApp1/Types/Ad.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Types
+{
+    public class Ad : TelevisionProgram, showInfo
+    {
+        public Ad(int duration, int ageRange, string name) : base(duration, ageRange, name) { }
+
+        public void showDuration()
+        {
+            Console.WriteLine("Ad goes for {0} minutes", duration);
+        }
+        public void editAgeRange(int ageRange = 0)
+        {
+            Console.WriteLine("This ad has no age limit!");
+        }
+    }
+}
