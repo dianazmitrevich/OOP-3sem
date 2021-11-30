8a27e6f3659bd954af57c05a784595f12b45be6a
dianazmitrevich <zmitrevichdiana@gmail.com>
2021-11-30 13:18:26 +0300

    created container class programGuide.cs, added methods

---
+++ лб 5/ConsoleApp1/Container (lb 6)/ProgramGuide.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Container__lb_6_
+{
+    public class ProgramGuide
+    {
+        // Создать Программу передач.
+        // Найти все фильмы, снятые в определенный год (с рейтингом более 5), подсчитать
+        // продолжительность программы по времени, число
+        // рекламных роликов.
+
+        public object[] programArray;
+
+        public ProgramGuide(params object[] array)
+        {
+            this.programArray = array;
+        }
+
+        public object this[int i]
+        {
+            get => programArray[i];
+            set => programArray.Append(value);
+        }
+
+        public void add(object element)
+        {
+            this.programArray = programArray.Append(element).ToArray();
+        }
+
+        public void del(int number)
+        {
+            this.programArray = programArray.Where((e, i) => i != number).ToArray();
+        }
+
+        public void show()
+        {
+            for (int i = 0; i < this.programArray.Length; i++)
+            {
+                Console.WriteLine(this.programArray[i]);
+            }
+        }
+    }
+}
