304abfcfcb4ec1a1efe5f5fd91502f01bdc48838
dianazmitrevich <zmitrevichdiana@gmail.com>
2021-11-30 12:45:11 +0300

    made news class partial, moved methods to newsPartial.cs

--- лб 5/ConsoleApp1/Types/News.cs
+++ лб 5/ConsoleApp1/Types/News.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1.Types
 {
-    public class News : TelevisionProgram, showInfo
+    public partial class News : TelevisionProgram, showInfo
     {
         public string category = "";
 
@@ -22,23 +22,6 @@ namespace ConsoleApp1.Types
             this.Duration = duration;
             this.AgeRange = ageRange;
             this.category = category;
-        }
-
-        public void showDuration()
-        {
-            Console.WriteLine("This news episode goes for {0} minutes", Duration);
-        }
-
-        public void editAgeRange(int ageRange = 0)
-        {
-            if (ageRange > 0)
-                this.AgeRange = ageRange;
-            else Console.WriteLine("Age range can't be less than 0");
-        }
-
-        public override string ToString()
-        {
-            return $"This news episode goes for {this.Duration} minutes \nAge limit is {this.AgeRange} and above years \nNews category is {this.category}";
         }
     }
 }
---
+++ лб 5/ConsoleApp1/Types/NewsPartial.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Types
+{
+    public partial class News : TelevisionProgram, showInfo
+    {
+        public void showDuration()
+        {
+            Console.WriteLine("This news episode goes for {0} minutes", Duration);
+        }
+
+        public void editAgeRange(int ageRange = 0)
+        {
+            if (ageRange > 0)
+                this.AgeRange = ageRange;
+            else Console.WriteLine("Age range can't be less than 0");
+        }
+
+        public override string ToString()
+        {
+            return $"This news episode goes for {this.Duration} minutes \nAge limit is {this.AgeRange} and above years \nNews category is {this.category}";
+        }
+    }
+}
