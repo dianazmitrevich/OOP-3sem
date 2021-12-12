0e847d92f62fa72dfd7b99030c6cc8c20fb543bd
dianazmitrevich <zmitrevichdiana@gmail.com>
2021-12-12 15:12:19 +0300

    checked the usability of events

--- лб 9/ConsoleApp1/Boss.cs
+++ лб 9/ConsoleApp1/Boss.cs
@@ -18,20 +18,34 @@ namespace ConsoleApp1
         // Проверить результаты изменения состояния объектов после
         // наступления событий, возможно не однократном.
 
+        public string Name { get; }
         public string Type { get; }
         public int Level { get; set; }
         public double Strain { get; set; }
 
-        public Boss(string type = "", int level = 0, double strain = 0.0)
+        public Boss(string name = "", string type = "", int level = 0, double strain = 0.0)
         {
+            this.Name = name;
             this.Type = type;
             this.Level = level;
             this.Strain = strain;
         }
 
-        public delegate void Upgrade(int upgradeTo);
-        public delegate void TurnOn(double currentStrain);
-        public event Upgrade UpgradeEvent;
-        public event TurnOn TurnOnEvent;
+        public delegate void UpgradeLevel(Boss machine, int upgradeTo);
+        public delegate void TurnOnMachine(Boss machine, double currentStrain);
+        public event UpgradeLevel UpgradeEvent;
+        public event TurnOnMachine TurnOnEvent;
+
+        public void Upgrade(int upgradeTo)
+        {
+            this.Level = this.Level + upgradeTo;
+            this.UpgradeEvent?.Invoke(this, upgradeTo);
+        }
+
+        public void TurnOn(double currentStrain)
+        {
+            this.Strain = currentStrain;
+            this.TurnOnEvent?.Invoke(this, currentStrain);
+        }
     }
 }
--- лб 9/ConsoleApp1/Program.cs
+++ лб 9/ConsoleApp1/Program.cs
@@ -10,6 +10,35 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Boss PersonWill = new Boss("PersonWill", "person", 2, 0);
+            Boss RobotWill = new Boss("RobotWill", "robot", 0, 0);
+
+            Boss.UpgradeLevel upgrade = (Boss machine, int level) => Console.WriteLine($"{machine.Name} was upgraded by {level} levels");
+            Boss.TurnOnMachine turnOn = (Boss machine, double currentStrain) => Console.WriteLine($"{machine.Name} trying to turn on with {currentStrain} strain...");
+
+            PersonWill.UpgradeEvent += upgrade;
+            PersonWill.TurnOnEvent += turnOn;
+
+            RobotWill.UpgradeEvent += upgrade;
+
+            PersonWill.Upgrade(5);
+            PersonWill.TurnOnEvent += StrainMaxCapacity;
+            PersonWill.TurnOn(10.7);
+            Console.WriteLine();
+
+            RobotWill.Upgrade(19);
+            RobotWill.TurnOn(17.23);
+
+            Console.ReadKey();
+        }
+
+        // обработчик максимального напряжения
+        private static void StrainMaxCapacity(Boss machine, double currentStrain)
+        {
+            if (machine.Type == "person" && currentStrain > 9.5)
+                Console.WriteLine($"{machine.Name} broke under this strain while trying to turn on!");
+            else if (machine.Type == "robot" && currentStrain > 24.2)
+                Console.WriteLine($"{machine.Name} broke under this strain while trying to turn on!");
         }
     }
 }
