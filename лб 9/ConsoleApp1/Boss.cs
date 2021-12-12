2ecd4471d843389eed404b48e667990ab8d95583
dianazmitrevich <zmitrevichdiana@gmail.com>
2021-12-12 14:08:57 +0300

    created delegates and events

--- лб 9/ConsoleApp1/Boss.cs
+++ лб 9/ConsoleApp1/Boss.cs
@@ -10,12 +10,28 @@ namespace ConsoleApp1
     {
         // Вариант 8
 
-        // Создать класс Boss с событиями Upgrade и Turn-on(под
+        // Создать класс Boss с событиями Upgrade и TurnOn(под
         // определенным напряжением, учтите что техника или человек могут
         // сломаться). В main создать некоторое количество объектов(техники
         // или полу-техники). Подпишите объекты на события произвольным
-        // образом.Реакция на события у разных объектов будет разная.
+        // образом. Реакция на события у разных объектов будет разная.
         // Проверить результаты изменения состояния объектов после
         // наступления событий, возможно не однократном.
+
+        public string Type { get; }
+        public int Level { get; set; }
+        public double Strain { get; set; }
+
+        public Boss(string type = "", int level = 0, double strain = 0.0)
+        {
+            this.Type = type;
+            this.Level = level;
+            this.Strain = strain;
+        }
+
+        public delegate void Upgrade(int upgradeTo);
+        public delegate void TurnOn(double currentStrain);
+        public event Upgrade UpgradeEvent;
+        public event TurnOn TurnOnEvent;
     }
 }
